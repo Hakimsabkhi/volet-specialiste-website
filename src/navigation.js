81bba129feb873dbdc1a4804b3e8babdd7a30412
Hakimsabkhi <62035010+Hakimsabkhi@users.noreply.github.com>
2023-09-01 16:34:16 +0100

    first commit

---
+++ src/navigation.js
@@ -0,0 +1,64 @@
+import { getPermalink } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'Accueil',
+      href: getPermalink('/'),
+    },
+    {
+      text: 'Type de volet roulant',
+      href: getPermalink('/type-volet-roulant'),
+    },
+    {
+      text: 'Tarifs et prix',
+      href: getPermalink('/tarifs'),
+    },
+    {
+      text: 'matériaux',
+      href: getPermalink('/materiaux'),
+    },
+    {
+      text: 'Devis gratuit',
+      href: '/devis-gratuit',
+    },
+  ],
+  actions: [
+    {
+      type: 'button',
+      class:
+        'text-white bg-[#2557D6] hover:bg-[#2557D6]/90 focus:ring-4 focus:ring-[#2557D6]/50 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#2557D6]/50 mr-2 mb-2',
+      text: 'Contactez-nous',
+      href: '/contact',
+    },
+  ],
+};
+
+export const footerData = {
+  links: [
+    {
+      title: 'INFOS LÉGALES',
+      links: [
+        { text: 'Conditions générales', href: '#' },
+        { text: 'Politique de vie privée', href: '#' },
+      ],
+    },
+    {
+      title: 'CONTACT',
+      links: [
+        { text: 'Qui sommes-nous ?', href: '#' },
+        { text: 'Contact', href: '#' },
+      ],
+    },
+  ],
+  secondaryLinks: [],
+  socialLinks: [
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Linkedin', icon: 'tabler:brand-linkedin', href: '#' },
+    { ariaLabel: 'Youtube', icon: 'tabler:brand-youtube', href: '#' },
+  ],
+  footNote: `
+    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="/"> © 2023 -Volet specialiste </a> · All rights reserved.
+  `,
+};
