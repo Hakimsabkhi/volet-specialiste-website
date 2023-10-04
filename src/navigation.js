1d3d9eb40363d899aca0ac848455ee880df2849b
Hakimsabkhi <62035010+Hakimsabkhi@users.noreply.github.com>
2023-10-04 15:30:15 +0100

    10/4/2023

--- src/navigation.js
+++ src/navigation.js
@@ -27,15 +27,15 @@ export const headerData = {
     {
       type: 'button',
       class:
-        'text-white bg-[var(--aw-color-secondary)] hover:bg-[#2557D6]/90 focus:ring-4 focus:ring-[#2557D6]/50 focus:outline-none font-medium rounded-4xl text-sm px-5 py-3 text-center inline-flex items-center 2xl:mr-24',
+        'btn btn-primary btn md:hidden text-white font-medium rounded-4xl text-sm px-5 py-3 text-center inline-flex items-center 2xl:mr-24',
       text: 'Appelez Nous',
       href: 'tel:01 85 49 07 07',
     },
     {
       type: 'button',
       class:
-        'md:hidden text-white bg-[var(--aw-color-secondary)] hover:bg-[#2557D6]/90 focus:ring-4 focus:ring-[#2557D6]/50 focus:outline-none font-medium rounded-4xl text-sm px-5 py-3 text-center inline-flex items-center 2xl:mr-24',
-      text: 'Contactez-nous',
+        'btn btn-primary text-white font-medium rounded-4xl text-sm px-5 py-3 text-center inline-flex items-center 2xl:mr-24',
+      text: 'Contactez Nous',
       href: '/contact',
     },
   ],
