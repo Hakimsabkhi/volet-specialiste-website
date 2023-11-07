7753e65d9e61525c68b9a351c286af55bc611dcd
Hakimsabkhi <hakimsabkhi001@gmail.com>
2023-11-07 18:45:25 +0100

    11/7/23

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -19,7 +19,7 @@ export const get = async () => {
     description: METADATA_CONFIG?.description || '',
     site: import.meta.env.SITE,
 
-    items: posts.flatMap((post) => ({
+    items: posts.Map((post) => ({
       link: getPermalink(post.permalink, 'post'),
       title: post.title,
       description: post.excerpt,
