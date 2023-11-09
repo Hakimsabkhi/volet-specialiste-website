7a2e04133eed058e4f23f04b638a2fb8c7400de2
Hakimsabkhi <hakimsabkhi001@gmail.com>
2023-11-09 20:48:22 +0100

    11/9/23

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,52 +7,77 @@ import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import compress from 'astro-compress';
 import icon from 'astro-icon';
-import tasks from "./src/utils/tasks";
+import tasks from './src/utils/tasks';
 import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
 import { ANALYTICS_CONFIG, SITE_CONFIG } from './src/utils/config.ts';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/static';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const whenExternalScripts = (items = []) => ANALYTICS_CONFIG.vendors.googleAnalytics.id && ANALYTICS_CONFIG.vendors.googleAnalytics.partytown ? Array.isArray(items) ? items.flatMap(item => item()) : [items()] : [];
+const whenExternalScripts = (items = []) =>
+  ANALYTICS_CONFIG.vendors.googleAnalytics.id && ANALYTICS_CONFIG.vendors.googleAnalytics.partytown
+    ? Array.isArray(items)
+      ? items.flatMap((item) => item())
+      : [items()]
+    : [];
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_CONFIG.site,
   base: SITE_CONFIG.base,
   trailingSlash: SITE_CONFIG.trailingSlash ? 'always' : 'never',
-  output: 'hybrid',
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), sitemap(), mdx(), icon({
-    include: {
-      tabler: ['*'],
-      'flat-color-icons': ['template', 'gallery', 'approval', 'document', 'advertising', 'currency-exchange', 'voice-presentation', 'business-contact', 'database']
-    }
-  }), ...whenExternalScripts(() => partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  })), tasks(), compress({
-    CSS: true,
-    HTML: {
-      removeAttributeQuotes: false
-    },
-    Image: false,
-    JavaScript: true,
-    SVG: true,
-    Logger: 1
-  })],
+  output: 'static',
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    mdx(),
+    icon({
+      include: {
+        tabler: ['*'],
+        'flat-color-icons': [
+          'template',
+          'gallery',
+          'approval',
+          'document',
+          'advertising',
+          'currency-exchange',
+          'voice-presentation',
+          'business-contact',
+          'database',
+        ],
+      },
+    }),
+    ...whenExternalScripts(() =>
+      partytown({
+        config: {
+          forward: ['dataLayer.push'],
+        },
+      })
+    ),
+    tasks(),
+    compress({
+      CSS: true,
+      HTML: {
+        removeAttributeQuotes: false,
+      },
+      Image: false,
+      JavaScript: true,
+      SVG: true,
+      Logger: 1,
+    }),
+  ],
   markdown: {
-    remarkPlugins: [readingTimeRemarkPlugin]
+    remarkPlugins: [readingTimeRemarkPlugin],
   },
   experimental: {
-    devOverlay: true
+    devOverlay: true,
   },
   vite: {
     resolve: {
       alias: {
-        '~': path.resolve(__dirname, './src')
-      }
-    }
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
   },
   adapter: vercel({
     webAnalytics: {
@@ -61,6 +86,5 @@ export default defineConfig({
     speedInsights: {
       enabled: true,
     },
-    
   }),
 });
