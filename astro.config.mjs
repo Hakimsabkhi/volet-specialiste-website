0ac6a71b08c17bece1fa4f3d9487d67874f5b364
Hakimsabkhi <hakimsabkhi001@gmail.com>
2024-01-10 08:27:35 +0100

    1/10/24

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
-import compress from 'astro-compress';
 import icon from 'astro-icon';
 import tasks from './src/utils/tasks';
 import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
@@ -55,16 +54,7 @@ export default defineConfig({
       })
     ),
     tasks(),
-    compress({
-      CSS: true,
-      HTML: {
-        removeAttributeQuotes: false,
-      },
-      Image: false,
-      JavaScript: true,
-      SVG: true,
-      Logger: 1,
-    }),
+    
   ],
   markdown: {
     remarkPlugins: [readingTimeRemarkPlugin],
